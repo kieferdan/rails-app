7c8cd0ecdbb9bc1fa6b093cb6c12e1a80914441c
Daniel Kiefer <danielfmpinho@gmail.com>
2023-10-25 16:47:56 -0300

    welcome controller routes view

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,14 @@
+class WelcomeController < ApplicationController
+  def home
+    @message = "Welcome to my homepage"
+  end
+
+  def features
+  end
+
+  def about
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/home'
+  get 'welcome/features'
+  get 'welcome/about'
+  get 'welcome/contact'
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
