46b321305c7a92dfc68a3f3245f75448279e7c7e
Daniel Kiefer <danielfmpinho@gmail.com>
2023-10-26 15:20:20 -0300

    controller routes

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,4 +2,13 @@ class PublicController < ApplicationController
   def homepage
     @posts = Post.all
   end
+
+  def about
+  end
+
+  def blog
+  end
+
+  def contact
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  get "about" => "public#about", as: :about
+  get "blog" => "public#blog", as: :blog
+  get "contact" => "public#contact", as: :contact
+
   # Defines the root path route ("/")
   root to: "public#homepage"
 end
