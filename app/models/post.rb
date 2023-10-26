cfee449211919862d5a40eef7838e1ec110359eb
Daniel Kiefer <danielfmpinho@gmail.com>
2023-10-26 18:28:27 -0300

    model comment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :summary, presence: true
+  validates :body, presence: true
+
+  def details
+    "This post was created on #{created_at.strftime("%d/%m/%Y")}"
+  end
+
+  def self.total
+    count
+  end
 end
--- test/models/c_ategory_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CAtegoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
